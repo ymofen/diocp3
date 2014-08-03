eba14bec0aae877219815d550be617a75cab7d53
mofen <185511468@qq.com>
2014-08-03 22:46:17 +0800

    + diocpTcpClientDemo.dpr

---
+++ samples/socket-Coder/Client/diocpTcpClientDemo.dpr
@@ -0,0 +1,23 @@
+program diocpTcpClientDemo;
+
+uses
+  Vcl.Forms,
+  ufrmMain in 'ufrmMain.pas' {frmMain},
+  iocpCoderTcpClient in '..\..\..\source\iocp-socket-coder\iocpCoderTcpClient.pas',
+  AnsiStringTools in '..\diocpCoders\AnsiStringTools.pas',
+  JSonStream in '..\diocpCoders\JSonStream.pas',
+  superobject in '..\diocpCoders\superobject.pas',
+  uCRCTools in '..\diocpCoders\uCRCTools.pas',
+  uIOCPJSonStreamDecoder in '..\diocpCoders\uIOCPJSonStreamDecoder.pas',
+  uIOCPJSonStreamEncoder in '..\diocpCoders\uIOCPJSonStreamEncoder.pas',
+  uMyTypes in '..\diocpCoders\uMyTypes.pas',
+  uZipTools in '..\diocpCoders\uZipTools.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
