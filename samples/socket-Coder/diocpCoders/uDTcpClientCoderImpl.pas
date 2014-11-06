c93e87d22e7843c4a1d6801963a909a0485ef73e
ymofen <185511468@qq.com>
2014-11-06 17:59:15 +0800

    + Add DTcpClientCoderImpl(cross plateform unit)

---
+++ samples/socket-Coder/diocpCoders/uDTcpClientCoderImpl.pas
@@ -0,0 +1,89 @@
+(*
+   unit owner: d10.ymofen
+
+   + add reconnect param in constructor
+     2014-10-11 22:18:18
+*)
+unit uDTcpClientCoderImpl;
+
+
+
+interface
+
+uses
+  uICoderSocket, DTcpClient;
+
+type
+  TRawTcpClientCoderImpl = class(TInterfacedObject, ICoderSocket)
+  private
+    FReconnect: Boolean;
+    FTcpClient: TDTcpClient;
+  protected
+    function sendBuf(buf:Pointer; len:Cardinal):Cardinal; stdcall;
+    function recvBuf(buf:Pointer; len:Cardinal):Cardinal; stdcall;    
+    procedure closeSocket; stdcall;
+  public
+    constructor Create(ATcpClient: TDTcpClient; pvReconnect: Boolean = true);
+    destructor Destroy; override;
+  end;
+
+implementation
+
+constructor TRawTcpClientCoderImpl.Create(ATcpClient: TDTcpClient; pvReconnect:
+    Boolean = true);
+begin
+  inherited Create;
+  FTcpClient := ATcpClient;
+  FReconnect := pvReconnect;
+end;
+
+destructor TRawTcpClientCoderImpl.Destroy;
+begin
+  inherited Destroy;
+end;
+
+{ TRawTcpClientCoderImpl }
+
+procedure TRawTcpClientCoderImpl.closeSocket;
+begin
+  FTcpClient.Disconnect;
+end;
+
+function TRawTcpClientCoderImpl.recvBuf(buf: Pointer; len: Cardinal): Cardinal;
+begin
+  if FReconnect then
+  begin
+    if not FTcpClient.Active then FTcpClient.connect;
+    try
+      FTcpClient.recv(buf, len);
+      Result := len;
+    except
+      FTcpClient.Disconnect;
+      raise;
+    end;
+  end else
+  begin
+     FTcpClient.recv(buf, len);
+     Result := len;
+  end;
+
+end;
+
+function TRawTcpClientCoderImpl.sendBuf(buf: Pointer; len: Cardinal): Cardinal;
+begin
+  if FReconnect then
+  begin
+    if not FTcpClient.Active then FTcpClient.connect;
+    try
+      Result := FTcpClient.sendBuffer(buf, len);
+    except
+      FTcpClient.Disconnect;
+      raise;
+    end;
+  end else
+  begin
+    Result := FTcpClient.sendBuffer(buf, len);
+  end;
+end;
+
+end.
